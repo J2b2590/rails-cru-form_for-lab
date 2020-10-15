ea2f35416bbede30a9f9c8951df874b662ba3987
J2b2590 <J2b2590@gmail.com>
2020-10-15 08:08:11 -0500

    cannot find artist_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+artist1 = Artist.create(name:"Jeff", bio:"bio1")
+artist2 = Artist.create(name:"don", bio:"bio2")
+artist3 = Artist.create(name:"jon", bio:"bio3")
+artist4 = Artist.create(name:"gong", bio:"bio4")
+artist5 = Artist.create(name:"gurt", bio:"bio5")
+
+
+genre1 = Genre.create(name:"Genre1")
+genre2 = Genre.create(name:"Genre2")
+genre3 = Genre.create(name:"Genre3")
+genre4 = Genre.create(name:"Genre4")
+genre5 = Genre.create(name:"Genre5")
+
+Song.create(name:"my hit1",artist_id: artist1.id, genre_id: genre1.id)
+Song.create(name:"my hit2",artist_id: artist2.id, genre_id: genre2.id)
+Song.create(name:"my hit3",artist_id: artist3.id, genre_id: genre3.id)
+Song.create(name:"my hit4",artist_id: artist4.id, genre_id: genre4.id)
+Song.create(name:"my hit5",artist_id: artist5.id, genre_id: genre5.id)
+
+
+
